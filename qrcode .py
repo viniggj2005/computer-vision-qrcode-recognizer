cfafacf15c9660f024f999821d7224bf5aba0f50
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2025-04-06 19:26:56 -0300

    requirements.txt

--- qrcode .py
+++ qrcode .py
@@ -3,7 +3,7 @@ import os
 import contextlib
 from ultralytics import YOLO
 
-modelo = YOLO('./qrcode.pt') 
+modelo = YOLO('./qrcode.pt')
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     exit()
@@ -14,12 +14,14 @@ try:
         if not ret:
             break
         with open(os.devnull, 'w') as f, contextlib.redirect_stdout(f), contextlib.redirect_stderr(f):
-            resultados = modelo(frame,verbose=False)
-
-        frame_anotado = resultados[0].plot()
-
-        cv2.imshow("Video", frame_anotado)
-
+            resultados = modelo(frame, verbose=False)
+        
+        for box in resultados[0].boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (3, 120, 255), 2)
+            cv2.putText(frame, 'QRCode', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+        
+        cv2.imshow("Qrcode recognizer", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
